86a9ac4b76035d21732112eb1694d8457701c929
Germain Haugou <germain.haugou@iie.ee.ethz.ch>
2018-02-07 16:12:10 +0100

    Few fixes

--- configs/pulpissimo-zeroriscy.sh
+++ configs/pulpissimo-zeroriscy.sh
@@ -4,4 +4,6 @@ scriptDir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
 export PULP_CURRENT_CONFIG=pulpissimo@user_config_file=${scriptDir}/json/pulpissimo-zeroriscy.json
 
+unset PULP_CURRENT_CONFIG_ARGS
+
 source ${scriptDir}/../init.sh
