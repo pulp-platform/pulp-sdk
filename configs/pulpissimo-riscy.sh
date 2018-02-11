6b46040d91274b8c26db79657d27a42659100f71
Germain Haugou <germain.haugou@iie.ee.ethz.ch>
2018-02-11 22:27:55 +0100

    Reworked json architecture description, removed the level below system

--- configs/pulpissimo-riscy.sh
+++ configs/pulpissimo-riscy.sh
@@ -2,7 +2,7 @@
 
 scriptDir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
-export PULP_CURRENT_CONFIG=pulpissimo@user_config_file=${scriptDir}/json/pulpissimo-riscy.json
+export PULP_CURRENT_CONFIG=pulpissimo-riscy@user_config_file=${scriptDir}/json/pulpissimo-riscy.json
 
 unset PULP_CURRENT_CONFIG_ARGS
 
