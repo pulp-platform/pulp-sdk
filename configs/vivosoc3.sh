6bb2b510f3dc93fe2d59bca20ce543a9b838281e
Germain Haugou <germain.haugou@iie.ee.ethz.ch>
2018-03-13 09:21:42 +0100

    Added mchan in new virtual platform

---
+++ configs/vivosoc3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+scriptDir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+export PULP_CURRENT_CONFIG=vivosoc3@user_config_file=${scriptDir}/json/vivosoc3.json
+
+unset PULP_CURRENT_CONFIG_ARGS
+
+source ${scriptDir}/../init.sh
