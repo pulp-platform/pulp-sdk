ffc72db4efc16c5c71eeb782ebf1a7d579c4b90e
Germain Haugou <germain.haugou@iis.ee.ethz.ch>
2018-06-04 20:56:45 +0200

    Finished adding oprecompkw support

---
+++ configs/oprecompkw.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+scriptDir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+export PULP_CURRENT_CONFIG=oprecompkw@user_config_file=${scriptDir}/json/oprecompkw.json
+
+unset PULP_CURRENT_CONFIG_ARGS
+
+if [ -e ${scriptDir}/../init.sh ]; then
+    source ${scriptDir}/../init.sh
+fi
