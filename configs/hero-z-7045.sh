75e3c2c3fd58b91f0c6a86a3a08d1884af3b7520
Germain Haugou <germain.haugou@iis.ee.ethz.ch>
2018-07-25 16:04:42 +0200

    Created hero-z-7045 config

---
+++ configs/hero-z-7045.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+scriptDir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+export PULP_CURRENT_CONFIG=hero-z-7045@config_file=${scriptDir}/json/hero-z-7045.json
+
+unset PULP_CURRENT_CONFIG_ARGS
+
+if [ -e ${scriptDir}/../init.sh ]; then
+    source ${scriptDir}/../init.sh
+fi
