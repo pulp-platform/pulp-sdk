faac73a75a25184022b897dde8ca9831ba3ae012
Germain Haugou <germain.haugou@iis.ee.ethz.ch>
2018-07-26 12:46:08 +0200

    Added oprecompkw_sfloat

---
+++ configs/oprecompkw_sfloat.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+scriptDir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+export PULP_CURRENT_CONFIG=oprecompkw_sfloat@config_file=${scriptDir}/json/oprecompkw_sfloat.json
+
+unset PULP_CURRENT_CONFIG_ARGS
+
+if [ -e ${scriptDir}/../init.sh ]; then
+    source ${scriptDir}/../init.sh
+fi
