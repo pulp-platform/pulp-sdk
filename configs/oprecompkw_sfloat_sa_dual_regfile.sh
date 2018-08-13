c229891563e8b73ca972e573fda3d1078106778a
Germain Haugou <germain.haugou@iis.ee.ethz.ch>
2018-08-13 10:42:20 +0200

    Forgot a few files

---
+++ configs/oprecompkw_sfloat_sa_dual_regfile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+scriptDir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+export PULP_CURRENT_CONFIG=oprecompkw_sfloat_sa_dual_regfile@config_file=${scriptDir}/json/oprecompkw_sfloat_sa_dual_regfile.json
+
+unset PULP_CURRENT_CONFIG_ARGS
+
+if [ -e ${scriptDir}/../init.sh ]; then
+    source ${scriptDir}/../init.sh
+fi
