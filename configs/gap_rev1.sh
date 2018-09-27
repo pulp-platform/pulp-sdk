52fe406908615ba41b820896fe2eff458d640e56
Germain Haugou <germain.haugou@iis.ee.ethz.ch>
2018-09-28 00:22:42 +0200

    Forgot few files

---
+++ configs/gap_rev1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+scriptDir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+export PULP_CURRENT_CONFIG=gap_rev1@config_file=${scriptDir}/json/gap_rev1.json
+
+unset PULP_CURRENT_CONFIG_ARGS
+
+if [ -e ${scriptDir}/../init.sh ]; then
+    source ${scriptDir}/../init.sh
+fi
