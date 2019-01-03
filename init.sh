0aad61b7b5cd2aeadeb9ba4193b794033f2aab96
Germain Haugou <germain.haugou@iis.ee.ethz.ch>
2019-01-03 14:00:47 +0100

    Fixed regression

--- init.sh
+++ init.sh
@@ -19,6 +19,7 @@ scriptDir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 export PULP_PROJECT_HOME="$scriptDir"
 echo "Configuring Pulp project at $PULP_PROJECT_HOME"
 add_path $PULP_PROJECT_HOME/pulp-tools/bin
+add_path $PULP_PROJECT_HOME/pulp-configs/bin
 add_PYTHONPATH $PULP_PROJECT_HOME/pulp-tools/python
 add_PYTHONPATH $PULP_PROJECT_HOME/pulp-configs/python
 add_PYTHONPATH $PULP_PROJECT_HOME/pulp-configs/bin
