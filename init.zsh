5877239fc8997a0c13b471325b8d79860dfe5230
Samuel Riedel <sriedel@iis.ee.ethz.ch>
2019-08-20 16:32:02 +0200

    Update .gitignore file and add ZSH init

---
+++ init.zsh
@@ -0,0 +1,27 @@
+#!/bin/bash -e
+
+add_path() {
+    export PATH=${PATH//":$1:"/:}
+    export PATH=${PATH/%":$1"/}
+    export PATH=${PATH/#"$1:"/}
+    export PATH="$1${PATH:+":$PATH"}"
+}
+
+add_PYTHONPATH() {
+    export PYTHONPATH=${PYTHONPATH//":$1:"/:}
+    export PYTHONPATH=${PYTHONPATH/%":$1"/}
+    export PYTHONPATH=${PYTHONPATH/#"$1:"/}
+    export PYTHONPATH="$1${PYTHONPATH:+":$PYTHONPATH"}"
+}
+
+scriptDir=${0:a:h}
+
+export PULP_PROJECT_HOME="$scriptDir"
+echo "Configuring Pulp project at $PULP_PROJECT_HOME"
+add_path $PULP_PROJECT_HOME/pulp-tools/bin
+add_path $PULP_PROJECT_HOME/pulp-configs/bin
+add_PYTHONPATH $PULP_PROJECT_HOME/pulp-tools/python
+add_PYTHONPATH $PULP_PROJECT_HOME/pulp-configs/python
+add_PYTHONPATH $PULP_PROJECT_HOME/pulp-configs/bin
+add_PYTHONPATH $PULP_PROJECT_HOME/json-tools/python
+export PULP_CONFIGS_PATH=$PULP_PROJECT_HOME/pulp-configs/configs
