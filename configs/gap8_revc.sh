cd32c076aa04fa5c807d43d37e7fb9fb141c9229
Germain Haugou <germain.haugou@iis.ee.ethz.ch>
2019-09-17 13:15:08 +0200

    Updated tests

--- configs/gap8_revc.sh
+++ configs/gap8_revc.sh
@@ -6,6 +6,6 @@ export PULP_CURRENT_CONFIG=gap8_revc@config_file=boards/gapuino_revc.json
 
 unset PULP_CURRENT_CONFIG_ARGS
 
-if [ -e ${scriptDir}/../../init.sh ]; then
-    source ${scriptDir}/../../init.sh
+if [ -e ${scriptDir}/../init.sh ]; then
+    source ${scriptDir}/../init.sh
 fi
