b17d2eae6039dbdc2c570b187dae9e50bd8c93be
Germain Haugou <germain.haugou@iis.ee.ethz.ch>
2019-09-17 18:57:42 +0200

    Added gapuino spi for gap_revb and gap_revc

---
+++ configs/boards/gapuino_spi_revb.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+scriptDir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+export PULP_CURRENT_CONFIG=gapuino_spi@config_file=boards/gapuino_spi_revb.json
+
+unset PULP_CURRENT_CONFIG_ARGS
+
+if [ -e ${scriptDir}/../../init.sh ]; then
+    source ${scriptDir}/../../init.sh
+fi
