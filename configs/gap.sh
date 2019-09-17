c6b7f3c756a1c8404ee08e3ce08df715b0802b77
Germain Haugou <germain.haugou@iis.ee.ethz.ch>
2019-09-17 16:01:38 +0200

    Fixed regression

--- configs/gap.sh
+++ configs/gap.sh
@@ -2,7 +2,7 @@
 
 scriptDir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
-export PULP_CURRENT_CONFIG=gap@config_file=boards/gapuino.json
+export PULP_CURRENT_CONFIG=gap@config_file=boards/gapuino_spi.json
 
 unset PULP_CURRENT_CONFIG_ARGS
 
