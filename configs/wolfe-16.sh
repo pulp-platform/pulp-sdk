f5214a2b2041b0312b2b10d15bd5d906d0f7e73f
Germain Haugou <germain.haugou@iis.ee.ethz.ch>
2019-12-13 22:33:51 +0100

    Added wolfe 16 cores

--- configs/wolfe-16.sh
+++ configs/wolfe-16.sh
@@ -2,13 +2,10 @@
 
 scriptDir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
-export PULP_CURRENT_CONFIG=pulp@config_file=chips/pulp/pulp.json
+export PULP_CURRENT_CONFIG=wolfe_16@config_file=chips/wolfe_16/wolfe_16.json
 
 unset PULP_CURRENT_CONFIG_ARGS
 
 if [ -e ${scriptDir}/../init.sh ]; then
     source ${scriptDir}/../init.sh
 fi
-
-export PULPOS_TARGET=pulp
-export PULPOS_SYSTEM=pulp
