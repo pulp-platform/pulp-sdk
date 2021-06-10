41e4de261ce3a11d9b72f3787628d245929da995
Alessandro Ottaviano <aottaviano@gransasso.ee.ethz.ch>
2021-06-10 11:40:56 +0200

    rtos: Decouple PULP and COREV toolchain targets with CV32E40P

--- rtos/pulpos/pulp/rules/pulpos/targets/pulp_cv32e40p.mk
+++ rtos/pulpos/pulp/rules/pulpos/targets/pulp_cv32e40p.mk
@@ -1,10 +1,14 @@
 CONFIG_NB_CLUSTER_PE ?= 8
 
-PULP_LDFLAGS      += 
-PULP_CFLAGS       +=  -D__riscv__ -DARCHI_HAS_COREV -DPLP_NO_BUILTIN
+# Define the toolchain under use with CV32E40P. Options are `PULP_TOOLCHAIN` or `COREV_TOOLCHAIN`
+TOOLCHAIN = PULP_TOOLCHAIN
+
+ifeq ($(TOOLCHAIN), COREV_TOOLCHAIN)
+PULP_LDFLAGS      +=
+PULP_CFLAGS       +=  -D__riscv__ -DARCHI_HAS_COREV -D__COREV_TOOLCHAIN__ -DPLP_NO_BUILTIN
 PULP_ARCH_CFLAGS ?=  -march=rv32imfc_xcorev
 PULP_ARCH_LDFLAGS ?=  -march=rv32imfc_xcorev
-PULP_ARCH_OBJDFLAGS ?= 
+PULP_ARCH_OBJDFLAGS ?=
 PULP_CFLAGS    += -fdata-sections -ffunction-sections -include pos/chips/pulp/config.h -I$(PULPOS_PULP_HOME)/include/pos/chips/pulp
 PULP_OMP_CFLAGS    += -fopenmp -mnativeomp
 PULP_LDFLAGS += -nostartfiles -nostdlib -Wl,--gc-sections -L$(PULPOS_PULP_HOME)/kernel -Tchips/pulp/link.ld -lgcc
@@ -13,6 +17,24 @@ PULP_CC = riscv32-corev-elf-gcc
 PULP_AR ?= riscv32-corev-elf-ar
 PULP_LD ?= riscv32-corev-elf-gcc
 PULP_OBJDUMP ?= riscv32-corev-elf-objdump
+else ifeq ($(TOOLCHAIN), PULP_TOOLCHAIN)
+PULP_LDFLAGS      +=
+PULP_CFLAGS       +=  -D__riscv__ -DARCHI_HAS_COREV -DARCHI_CORE_HAS_PULPV2 -D__PULP_TOOLCHAIN__
+PULP_ARCH_CFLAGS ?=  -march=rv32imcxgap9 -mPE=$(CONFIG_NB_CLUSTER_PE) -mFC=1
+PULP_ARCH_LDFLAGS ?=  -march=rv32imcxgap9 -mPE=$(CONFIG_NB_CLUSTER_PE) -mFC=1
+PULP_ARCH_OBJDFLAGS ?= -Mmarch=rv32imcxgap9
+PULP_CFLAGS    += -fdata-sections -ffunction-sections -include pos/chips/pulp/config.h -I$(PULPOS_PULP_HOME)/include/pos/chips/pulp -I$(PULP_EXT_LIBS)/include
+ifeq '$(CONFIG_OPENMP)' '1'
+	PULP_CFLAGS    += -fopenmp -mnativeomp
+endif
+PULP_OMP_CFLAGS    += -fopenmp -mnativeomp
+PULP_LDFLAGS += -nostartfiles -nostdlib -Wl,--gc-sections -L$(PULP_EXT_LIBS) -L$(PULPOS_PULP_HOME)/kernel -Tchips/pulp/link.ld -lgcc
+
+PULP_CC = riscv32-unknown-elf-gcc
+PULP_AR ?= riscv32-unknown-elf-ar
+PULP_LD ?= riscv32-unknown-elf-gcc
+PULP_OBJDUMP ?= riscv32-unknown-elf-objdump
+endif
 
 fc/archi=riscv
 pe/archi=riscv
