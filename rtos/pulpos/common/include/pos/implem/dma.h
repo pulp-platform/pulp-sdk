e869249a24566b3b5c97ff8c178e78712ca8996a
Michael Rogenmoser <michael@rogenmoser.us>
2022-01-31 19:20:02 +0100

    make dma copy.id=-1 when calling free

--- rtos/pulpos/common/include/pos/implem/dma.h
+++ rtos/pulpos/common/include/pos/implem/dma.h
@@ -44,6 +44,7 @@ static inline void __cl_dma_wait_safe(pi_cl_dma_cmd_t *copy)
   }
 
   plp_dma_counter_free(counter);
+  copy->id = -1;
 }
 
 
@@ -60,6 +61,7 @@ static inline void __cl_dma_wait(pi_cl_dma_cmd_t *copy)
   }
 
   plp_dma_counter_free(counter);
+  copy->id = -1;
 
   eu_mutex_unlock_from_id(0);
 }
@@ -69,7 +71,7 @@ static inline void __cl_dma_memcpy(unsigned int ext, unsigned int loc, unsigned 
 {
   eu_mutex_lock_from_id(0);
   
-  int id = -1;
+  int id = copy->id;
   if (!merge) id = plp_dma_counter_alloc();
   unsigned int cmd = plp_dma_getCmd(dir, size, PLP_DMA_1D, PLP_DMA_TRIG_EVT, PLP_DMA_NO_TRIG_IRQ, PLP_DMA_SHARED);
   // Prevent the compiler from pushing the transfer before all previous
@@ -84,7 +86,7 @@ static inline void __cl_dma_memcpy(unsigned int ext, unsigned int loc, unsigned 
 
 static inline void __cl_dma_memcpy_safe(unsigned int ext, unsigned int loc, unsigned short size, pi_cl_dma_dir_e dir, int merge, pi_cl_dma_cmd_t *copy)
 {
-  int id = -1;
+  int id = copy->id;
   if (!merge) id = plp_dma_counter_alloc();
   unsigned int cmd = plp_dma_getCmd(dir, size, PLP_DMA_1D, PLP_DMA_TRIG_EVT, PLP_DMA_NO_TRIG_IRQ, PLP_DMA_SHARED);
   // Prevent the compiler from pushing the transfer before all previous
@@ -131,17 +133,15 @@ static inline void __cl_dma_memcpy_2d(unsigned int ext, unsigned int loc, unsign
 {
   eu_mutex_lock_from_id(0);
   
-  int id = -1;
+  int id = copy->id;
   if (!merge) id = plp_dma_counter_alloc();
 
-  {
-    unsigned int cmd = plp_dma_getCmd(dir, size, PLP_DMA_2D, PLP_DMA_TRIG_EVT, PLP_DMA_NO_TRIG_IRQ, PLP_DMA_SHARED);
-    // Prevent the compiler from pushing the transfer before all previous
-    // stores are done
-    __asm__ __volatile__ ("" : : : "memory");
-    plp_dma_cmd_push_2d(cmd, loc, ext, stride, length);
-    if (!merge) copy->id = id;
-  }
+  unsigned int cmd = plp_dma_getCmd(dir, size, PLP_DMA_2D, PLP_DMA_TRIG_EVT, PLP_DMA_NO_TRIG_IRQ, PLP_DMA_SHARED);
+  // Prevent the compiler from pushing the transfer before all previous
+  // stores are done
+  __asm__ __volatile__ ("" : : : "memory");
+  plp_dma_cmd_push_2d(cmd, loc, ext, stride, length);
+  if (!merge) copy->id = id;
 
   eu_mutex_unlock_from_id(0);
 }
