2305336e89178fc729d75fe2c9335e8868c51f25
Arpan Prasad <prasadar@iis.ee.ethz.ch>
2022-05-22 20:36:49 +0200

    modified name

--- tests/neureka_L1_patch/neureka_test.c
+++ tests/neureka_L1_patch/neureka_test.c
@@ -28,47 +28,47 @@
 
 #include "hal_neureka.h"
 
-#include "streamin_fs1_output_64x16x16_input_32x16x16/inc/ne16_cfg.h"
-#include "streamin_fs1_output_64x16x16_input_32x16x16/inc/ne16_infeat.h"
-#include "streamin_fs1_output_64x16x16_input_32x16x16/inc/ne16_weights.h"
-#include "streamin_fs1_output_64x16x16_input_32x16x16/inc/ne16_scale.h"
-#include "streamin_fs1_output_64x16x16_input_32x16x16/inc/ne16_scale_bias.h"
-#include "streamin_fs1_output_64x16x16_input_32x16x16/inc/ne16_scale_shift.h"
-#include "streamin_fs1_output_64x16x16_input_32x16x16/inc/ne16_streamin.h"
-#include "streamin_fs1_output_64x16x16_input_32x16x16/inc/ne16_outfeat.h"
+#include "streamin_fs1_output_64x16x16_input_32x16x16/inc/neureka_cfg.h"
+#include "streamin_fs1_output_64x16x16_input_32x16x16/inc/neureka_infeat.h"
+#include "streamin_fs1_output_64x16x16_input_32x16x16/inc/neureka_weights.h"
+#include "streamin_fs1_output_64x16x16_input_32x16x16/inc/neureka_scale.h"
+#include "streamin_fs1_output_64x16x16_input_32x16x16/inc/neureka_scale_bias.h"
+#include "streamin_fs1_output_64x16x16_input_32x16x16/inc/neureka_scale_shift.h"
+#include "streamin_fs1_output_64x16x16_input_32x16x16/inc/neureka_streamin.h"
+#include "streamin_fs1_output_64x16x16_input_32x16x16/inc/neureka_outfeat.h"
 
-#include "no_normquant_stride2_output_64x16x16_input_64x32x32/inc/ne16_cfg.h"
-#include "no_normquant_stride2_output_64x16x16_input_64x32x32/inc/ne16_infeat.h"
-#include "no_normquant_stride2_output_64x16x16_input_64x32x32/inc/ne16_weights.h"
-#include "no_normquant_stride2_output_64x16x16_input_64x32x32/inc/ne16_scale.h"
-#include "no_normquant_stride2_output_64x16x16_input_64x32x32/inc/ne16_scale_bias.h"
-#include "no_normquant_stride2_output_64x16x16_input_64x32x32/inc/ne16_scale_shift.h"
-#include "no_normquant_stride2_output_64x16x16_input_64x32x32/inc/ne16_streamin.h"
+#include "no_normquant_stride2_output_64x16x16_input_64x32x32/inc/neureka_cfg.h"
+#include "no_normquant_stride2_output_64x16x16_input_64x32x32/inc/neureka_infeat.h"
+#include "no_normquant_stride2_output_64x16x16_input_64x32x32/inc/neureka_weights.h"
+#include "no_normquant_stride2_output_64x16x16_input_64x32x32/inc/neureka_scale.h"
+#include "no_normquant_stride2_output_64x16x16_input_64x32x32/inc/neureka_scale_bias.h"
+#include "no_normquant_stride2_output_64x16x16_input_64x32x32/inc/neureka_scale_shift.h"
+#include "no_normquant_stride2_output_64x16x16_input_64x32x32/inc/neureka_streamin.h"
 
-#include "avgpool_fs3_dw_output_64x32x32_input_64x32x32/inc/ne16_cfg.h"
-#include "avgpool_fs3_dw_output_64x32x32_input_64x32x32/inc/ne16_infeat.h"
-#include "avgpool_fs3_dw_output_64x32x32_input_64x32x32/inc/ne16_weights.h"
-#include "avgpool_fs3_dw_output_64x32x32_input_64x32x32/inc/ne16_scale.h"
-#include "avgpool_fs3_dw_output_64x32x32_input_64x32x32/inc/ne16_scale_bias.h"
-#include "avgpool_fs3_dw_output_64x32x32_input_64x32x32/inc/ne16_scale_shift.h"
-#include "avgpool_fs3_dw_output_64x32x32_input_64x32x32/inc/ne16_streamin.h"
+#include "avgpool_fs3_dw_output_64x32x32_input_64x32x32/inc/neureka_cfg.h"
+#include "avgpool_fs3_dw_output_64x32x32_input_64x32x32/inc/neureka_infeat.h"
+#include "avgpool_fs3_dw_output_64x32x32_input_64x32x32/inc/neureka_weights.h"
+#include "avgpool_fs3_dw_output_64x32x32_input_64x32x32/inc/neureka_scale.h"
+#include "avgpool_fs3_dw_output_64x32x32_input_64x32x32/inc/neureka_scale_bias.h"
+#include "avgpool_fs3_dw_output_64x32x32_input_64x32x32/inc/neureka_scale_shift.h"
+#include "avgpool_fs3_dw_output_64x32x32_input_64x32x32/inc/neureka_streamin.h"
 
-#include "padding_1111_fs3_output_32x16x16_input_32x16x16/inc/ne16_cfg.h"
-#include "padding_1111_fs3_output_32x16x16_input_32x16x16/inc/ne16_infeat.h"
-#include "padding_1111_fs3_output_32x16x16_input_32x16x16/inc/ne16_weights.h"
-#include "padding_1111_fs3_output_32x16x16_input_32x16x16/inc/ne16_scale.h"
-#include "padding_1111_fs3_output_32x16x16_input_32x16x16/inc/ne16_scale_bias.h"
-#include "padding_1111_fs3_output_32x16x16_input_32x16x16/inc/ne16_scale_shift.h"
-#include "padding_1111_fs3_output_32x16x16_input_32x16x16/inc/ne16_streamin.h"
+#include "padding_1111_fs3_output_32x16x16_input_32x16x16/inc/neureka_cfg.h"
+#include "padding_1111_fs3_output_32x16x16_input_32x16x16/inc/neureka_infeat.h"
+#include "padding_1111_fs3_output_32x16x16_input_32x16x16/inc/neureka_weights.h"
+#include "padding_1111_fs3_output_32x16x16_input_32x16x16/inc/neureka_scale.h"
+#include "padding_1111_fs3_output_32x16x16_input_32x16x16/inc/neureka_scale_bias.h"
+#include "padding_1111_fs3_output_32x16x16_input_32x16x16/inc/neureka_scale_shift.h"
+#include "padding_1111_fs3_output_32x16x16_input_32x16x16/inc/neureka_streamin.h"
 
-#include "stride2_fs1_output_32x16x16_input_64x32x32/inc/ne16_cfg.h"
-#include "stride2_fs1_output_32x16x16_input_64x32x32/inc/ne16_infeat.h"
-#include "stride2_fs1_output_32x16x16_input_64x32x32/inc/ne16_weights.h"
-#include "stride2_fs1_output_32x16x16_input_64x32x32/inc/ne16_scale.h"
-#include "stride2_fs1_output_32x16x16_input_64x32x32/inc/ne16_scale_bias.h"
-#include "stride2_fs1_output_32x16x16_input_64x32x32/inc/ne16_scale_shift.h"
-#include "stride2_fs1_output_32x16x16_input_64x32x32/inc/ne16_streamin.h"
-#include "stride2_fs1_output_32x16x16_input_64x32x32/inc/ne16_outfeat.h"
+#include "stride2_fs1_output_32x16x16_input_64x32x32/inc/neureka_cfg.h"
+#include "stride2_fs1_output_32x16x16_input_64x32x32/inc/neureka_infeat.h"
+#include "stride2_fs1_output_32x16x16_input_64x32x32/inc/neureka_weights.h"
+#include "stride2_fs1_output_32x16x16_input_64x32x32/inc/neureka_scale.h"
+#include "stride2_fs1_output_32x16x16_input_64x32x32/inc/neureka_scale_bias.h"
+#include "stride2_fs1_output_32x16x16_input_64x32x32/inc/neureka_scale_shift.h"
+#include "stride2_fs1_output_32x16x16_input_64x32x32/inc/neureka_streamin.h"
+#include "stride2_fs1_output_32x16x16_input_64x32x32/inc/neureka_outfeat.h"
 
 
 #define NB_ITER 10
@@ -81,38 +81,31 @@ static int glob_errors;
 
 int run_test() {
 
-  // uint8_t* W0l        = ne16_weights_stride2;
-  uint8_t* nq0l       = ne16_scale_stride2;
-  uint8_t* nqs0l      = ne16_scale_shift_stride2;
-  uint8_t* nqb0l      = ne16_scale_bias_stride2;
-  uint8_t* actual_y0l = ne16_streamin_stride2;
+  uint8_t* nq0l       = neureka_scale_stride2;
+  uint8_t* nqs0l      = neureka_scale_shift_stride2;
+  uint8_t* nqb0l      = neureka_scale_bias_stride2;
+  uint8_t* actual_y0l = neureka_streamin_stride2;
 
-  // uint8_t* x1l        = ne16_cfg_padding;
-  // uint8_t* W1l        = ne16_weights_padding;
-  uint8_t* nq1l       = ne16_scale_padding;
-  uint8_t* nqs1l      = ne16_scale_shift_padding;
-  uint8_t* nqb1l      = ne16_scale_bias_padding;
-  uint8_t* actual_y1l = ne16_streamin_padding;
+  uint8_t* nq1l       = neureka_scale_padding;
+  uint8_t* nqs1l      = neureka_scale_shift_padding;
+  uint8_t* nqb1l      = neureka_scale_bias_padding;
+  uint8_t* actual_y1l = neureka_streamin_padding;
 
-  uint8_t* x0        = ne16_infeat_avgpool;
-  uint8_t* W0        = ne16_weights_avgpool;
-  uint8_t* nq0       = ne16_scale_avgpool;
-  uint8_t* nqs0      = ne16_scale_shift_avgpool;
-  uint8_t* nqb0      = ne16_scale_bias_avgpool;
-  uint8_t* actual_y0 = ne16_streamin_avgpool;
+  uint8_t* x0        = neureka_infeat_avgpool;
+  uint8_t* W0        = neureka_weights_avgpool;
+  uint8_t* nq0       = neureka_scale_avgpool;
+  uint8_t* nqs0      = neureka_scale_shift_avgpool;
+  uint8_t* nqb0      = neureka_scale_bias_avgpool;
+  uint8_t* actual_y0 = neureka_streamin_avgpool;
 
-  // uint8_t* x1        = ne16_infeat_no_normquant;
-  // uint8_t* W1        = ne16_weights_no_normquant;
-  uint8_t* nq1       = ne16_scale_no_normquant;
-  uint8_t* nqs1      = ne16_scale_shift_no_normquant;
-  uint8_t* nqb1      = ne16_scale_bias_no_normquant;
-  uint8_t* actual_y1 = ne16_streamin_no_normquant;
+  uint8_t* nq1       = neureka_scale_no_normquant;
+  uint8_t* nqs1      = neureka_scale_shift_no_normquant;
+  uint8_t* nqb1      = neureka_scale_bias_no_normquant;
+  uint8_t* actual_y1 = neureka_streamin_no_normquant;
 
-  // uint8_t* x2        = ne16_infeat_streamin;
-  // uint8_t* W2        = ne16_weights_streamin;
-  uint8_t* nq2       = ne16_scale_streamin;
-  uint8_t* nqs2      = ne16_scale_shift_streamin;
-  uint8_t* nqb2      = ne16_scale_bias_streamin;
+  uint8_t* nq2       = neureka_scale_streamin;
+  uint8_t* nqs2      = neureka_scale_shift_streamin;
+  uint8_t* nqb2      = neureka_scale_bias_streamin;
 
 
   // enable clock
@@ -126,18 +119,19 @@ int run_test() {
   // soft-clear NEUREKA
   NEUREKA_WRITE_CMD(NEUREKA_SOFT_CLEAR, NEUREKA_SOFT_CLEAR_ALL);
   for(volatile int kk=0; kk<10; kk++);
-// /*
   // program NEUREKA
+
+  // Stride2 pointwise 
   int job_id = -1;
   NEUREKA_BARRIER_ACQUIRE(job_id);
-  NEUREKA_WRITE_REG(NEUREKA_REG_WEIGHTS_PTR,     NEUREKA_REG_WEIGHTS_PTR+WEIGHT_MEM_BASE+sizeof(ne16_weights_no_normquant)+sizeof(ne16_weights_avgpool)+sizeof(ne16_weights_streamin));
+  NEUREKA_WRITE_REG(NEUREKA_REG_WEIGHTS_PTR,     NEUREKA_REG_WEIGHTS_PTR+WEIGHT_MEM_BASE+sizeof(neureka_weights_no_normquant)+sizeof(neureka_weights_avgpool)+sizeof(neureka_weights_streamin));
   NEUREKA_WRITE_REG(NEUREKA_REG_INFEAT_PTR,      x0);
   NEUREKA_WRITE_REG(NEUREKA_REG_OUTFEAT_PTR,     actual_y0l);
   NEUREKA_WRITE_REG(NEUREKA_REG_SCALE_PTR,       nq0l);
   NEUREKA_WRITE_REG(NEUREKA_REG_SCALE_SHIFT_PTR, nqs0l);
   NEUREKA_WRITE_REG(NEUREKA_REG_SCALE_BIAS_PTR,  nqb0l);
   for(int i=6; i<24; i++) {
-    NEUREKA_WRITE_REG(i*4, ne16_cfg_stride2[i]);
+    NEUREKA_WRITE_REG(i*4, neureka_cfg_stride2[i]);
   }
 
   // configure & reset perf counters
@@ -153,19 +147,20 @@ int run_test() {
   NEUREKA_WRITE_CMD(NEUREKA_COMMIT_AND_TRIGGER, NEUREKA_TRIGGER_CMD);
   // printf("TRIGGERED NEUREKA \n");
   
-
+  // 3x3 convolution with padding
   NEUREKA_BARRIER_ACQUIRE(job_id);
-  NEUREKA_WRITE_REG(NEUREKA_REG_WEIGHTS_PTR,     NEUREKA_REG_WEIGHTS_PTR+WEIGHT_MEM_BASE+sizeof(ne16_weights_no_normquant)+sizeof(ne16_weights_avgpool)+sizeof(ne16_weights_stride2)+sizeof(ne16_weights_streamin));
+  NEUREKA_WRITE_REG(NEUREKA_REG_WEIGHTS_PTR,     NEUREKA_REG_WEIGHTS_PTR+WEIGHT_MEM_BASE+sizeof(neureka_weights_no_normquant)+sizeof(neureka_weights_avgpool)+sizeof(neureka_weights_stride2)+sizeof(neureka_weights_streamin));
   NEUREKA_WRITE_REG(NEUREKA_REG_INFEAT_PTR,      actual_y0l-32*17);//offsetting to help padding 
   NEUREKA_WRITE_REG(NEUREKA_REG_OUTFEAT_PTR,     actual_y1l);
   NEUREKA_WRITE_REG(NEUREKA_REG_SCALE_PTR,       nq1l);
   NEUREKA_WRITE_REG(NEUREKA_REG_SCALE_SHIFT_PTR, nqs1l);
   NEUREKA_WRITE_REG(NEUREKA_REG_SCALE_BIAS_PTR,  nqb1l);
   for(int i=6; i<24; i++) {
-    NEUREKA_WRITE_REG(i*4, ne16_cfg_padding[i]);
+    NEUREKA_WRITE_REG(i*4, neureka_cfg_padding[i]);
   }
   NEUREKA_WRITE_CMD(NEUREKA_COMMIT_AND_TRIGGER, NEUREKA_TRIGGER_CMD);
 
+  // Average Pool
   NEUREKA_BARRIER_ACQUIRE(job_id);
   NEUREKA_WRITE_REG(NEUREKA_REG_WEIGHTS_PTR,     NEUREKA_REG_WEIGHTS_PTR+WEIGHT_MEM_BASE);
   NEUREKA_WRITE_REG(NEUREKA_REG_INFEAT_PTR,      x0);
@@ -174,31 +169,33 @@ int run_test() {
   NEUREKA_WRITE_REG(NEUREKA_REG_SCALE_SHIFT_PTR, nqs0);
   NEUREKA_WRITE_REG(NEUREKA_REG_SCALE_BIAS_PTR,  nqb0);
   for(int i=6; i<24; i++) {
-    NEUREKA_WRITE_REG(i*4, ne16_cfg_avgpool[i]);
+    NEUREKA_WRITE_REG(i*4, neureka_cfg_avgpool[i]);
   }
   NEUREKA_WRITE_CMD(NEUREKA_COMMIT_AND_TRIGGER, NEUREKA_TRIGGER_CMD);
 
+  // Pointwise Without Normquant
   NEUREKA_BARRIER_ACQUIRE(job_id);
-  NEUREKA_WRITE_REG(NEUREKA_REG_WEIGHTS_PTR,     NEUREKA_REG_WEIGHTS_PTR+WEIGHT_MEM_BASE+sizeof(ne16_weights_avgpool));
+  NEUREKA_WRITE_REG(NEUREKA_REG_WEIGHTS_PTR,     NEUREKA_REG_WEIGHTS_PTR+WEIGHT_MEM_BASE+sizeof(neureka_weights_avgpool));
   NEUREKA_WRITE_REG(NEUREKA_REG_INFEAT_PTR,      actual_y0);
   NEUREKA_WRITE_REG(NEUREKA_REG_OUTFEAT_PTR,     actual_y1);
   NEUREKA_WRITE_REG(NEUREKA_REG_SCALE_PTR,       nq1);
   NEUREKA_WRITE_REG(NEUREKA_REG_SCALE_SHIFT_PTR, nqs1);
   NEUREKA_WRITE_REG(NEUREKA_REG_SCALE_BIAS_PTR,  nqb1);
   for(int i=6; i<24; i++) {
-    NEUREKA_WRITE_REG(i*4, ne16_cfg_no_normquant[i]);
+    NEUREKA_WRITE_REG(i*4, neureka_cfg_no_normquant[i]);
   }
   NEUREKA_WRITE_CMD(NEUREKA_COMMIT_AND_TRIGGER, NEUREKA_TRIGGER_CMD);
 
+  // Pointwise with streamin
   NEUREKA_BARRIER_ACQUIRE(job_id);
-  NEUREKA_WRITE_REG(NEUREKA_REG_WEIGHTS_PTR,     NEUREKA_REG_WEIGHTS_PTR+WEIGHT_MEM_BASE+sizeof(ne16_weights_avgpool)+sizeof(ne16_weights_no_normquant));
+  NEUREKA_WRITE_REG(NEUREKA_REG_WEIGHTS_PTR,     NEUREKA_REG_WEIGHTS_PTR+WEIGHT_MEM_BASE+sizeof(neureka_weights_avgpool)+sizeof(neureka_weights_no_normquant));
   NEUREKA_WRITE_REG(NEUREKA_REG_INFEAT_PTR,      actual_y1l);
   NEUREKA_WRITE_REG(NEUREKA_REG_OUTFEAT_PTR,     actual_y1);
   NEUREKA_WRITE_REG(NEUREKA_REG_SCALE_PTR,       nq2);
   NEUREKA_WRITE_REG(NEUREKA_REG_SCALE_SHIFT_PTR, nqs2);
   NEUREKA_WRITE_REG(NEUREKA_REG_SCALE_BIAS_PTR,  nqb2);
   for(int i=6; i<24; i++) {
-    NEUREKA_WRITE_REG(i*4, ne16_cfg_streamin[i]);
+    NEUREKA_WRITE_REG(i*4, neureka_cfg_streamin[i]);
   }
   NEUREKA_WRITE_CMD(NEUREKA_COMMIT_AND_TRIGGER, NEUREKA_TRIGGER_CMD);
 
@@ -213,9 +210,8 @@ int run_test() {
 
   printf("%d cycles\n", pi_perf_read(PI_PERF_CYCLES));
 
-  int errors = neureka_compare_int(actual_y1, ne16_outfeat_streamin, STIM_Y_SIZE_STREAMIN/4);
+  volatile int errors = neureka_compare_int(actual_y1, neureka_outfeat_streamin, STIM_Y_SIZE_STREAMIN/4);
   return errors;
-  // return 0;
 }
 
 static struct pi_cluster_task task[1];
@@ -252,33 +248,26 @@ static int launch_cluster_task() {
 }
 
 int test_entry() { 
-  uint8_t* W0l       = ne16_weights_stride2;
-  uint8_t* W1l       = ne16_weights_padding;
-  uint8_t* W0        = ne16_weights_avgpool;
-  uint8_t* W1        = ne16_weights_no_normquant;
-  uint8_t* W2        = ne16_weights_streamin;
+  uint8_t* W0l       = neureka_weights_stride2;
+  uint8_t* W1l       = neureka_weights_padding;
+  uint8_t* W0        = neureka_weights_avgpool;
+  uint8_t* W1        = neureka_weights_no_normquant;
+  uint8_t* W2        = neureka_weights_streamin;
 
   uint32_t* weight_start_ptr = WEIGHT_MEM_BASE; 
-  memcpy(weight_start_ptr,(uint32_t*)ne16_weights_avgpool,sizeof(ne16_weights_avgpool));
-
-  printf("DONE MEMCPY 0\n");
+  memcpy(weight_start_ptr,(uint32_t*)neureka_weights_avgpool,sizeof(neureka_weights_avgpool));
   
-  weight_start_ptr = WEIGHT_MEM_BASE+SRAM_OFFSET+sizeof(ne16_weights_avgpool); 
-  memcpy(weight_start_ptr,(uint32_t*)ne16_weights_no_normquant,sizeof(ne16_weights_no_normquant));
-  printf("DONE MEMCPY 1\n");
+  weight_start_ptr = WEIGHT_MEM_BASE+SRAM_OFFSET+sizeof(neureka_weights_avgpool); 
+  memcpy(weight_start_ptr,(uint32_t*)neureka_weights_no_normquant,sizeof(neureka_weights_no_normquant));
   
-  weight_start_ptr = WEIGHT_MEM_BASE+SRAM_OFFSET+sizeof(ne16_weights_no_normquant)+sizeof(ne16_weights_avgpool); 
-  memcpy(weight_start_ptr,(uint32_t*)ne16_weights_streamin,sizeof(ne16_weights_streamin));
-  printf("DONE MEMCPY 2\n");
+  weight_start_ptr = WEIGHT_MEM_BASE+SRAM_OFFSET+sizeof(neureka_weights_no_normquant)+sizeof(neureka_weights_avgpool); 
+  memcpy(weight_start_ptr,(uint32_t*)neureka_weights_streamin,sizeof(neureka_weights_streamin));
 
-  weight_start_ptr = WEIGHT_MEM_BASE+SRAM_OFFSET+sizeof(ne16_weights_no_normquant)+sizeof(ne16_weights_avgpool)+sizeof(ne16_weights_streamin); 
-  memcpy(weight_start_ptr,(uint32_t*)ne16_weights_stride2,sizeof(ne16_weights_stride2));
-  printf("DONE MEMCPY 3\n");
+  weight_start_ptr = WEIGHT_MEM_BASE+SRAM_OFFSET+sizeof(neureka_weights_no_normquant)+sizeof(neureka_weights_avgpool)+sizeof(neureka_weights_streamin); 
+  memcpy(weight_start_ptr,(uint32_t*)neureka_weights_stride2,sizeof(neureka_weights_stride2));
 
-  weight_start_ptr = WEIGHT_MEM_BASE+SRAM_OFFSET+sizeof(ne16_weights_no_normquant)+sizeof(ne16_weights_avgpool)+sizeof(ne16_weights_stride2)+sizeof(ne16_weights_streamin); 
-  memcpy(weight_start_ptr,(uint32_t*)ne16_weights_padding,sizeof(ne16_weights_padding));
-  printf("DONE MEMCPY 4\n");
-
+  weight_start_ptr = WEIGHT_MEM_BASE+SRAM_OFFSET+sizeof(neureka_weights_no_normquant)+sizeof(neureka_weights_avgpool)+sizeof(neureka_weights_stride2)+sizeof(neureka_weights_streamin); 
+  memcpy(weight_start_ptr,(uint32_t*)neureka_weights_padding,sizeof(neureka_weights_padding));
 
   volatile int errors = launch_cluster_task();
   
---
+++ tests/neureka_L1_patch/no_normquant_stride2_output_64x16x16_input_64x32x32/inc/neureka_scale_bias.h
@@ -0,0 +1,3 @@
+#define STIM_NQB_SIZE 4
+// scale_bias [64]
+PI_L1 int32_t neureka_scale_bias_no_normquant[] = { 0 };
