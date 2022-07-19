11dc0e264acb16de303c4d89e15a70b2c6a1d48b
Arpan Prasad <prasadar@iis.ee.ethz.ch>
2022-07-19 10:53:58 +0200

    pusing the changes needs to check the regression but works with dory for 1x1 mode with smaller patches

--- tools/gvsoc/pulp/models/pulp/neureka/src/neureka_debug.cpp
+++ tools/gvsoc/pulp/models/pulp/neureka/src/neureka_debug.cpp
@@ -23,23 +23,31 @@
 
 void Neureka::debug_x_buffer() {
     std::ostringstream stringStream;
-    stringStream << "x_buffer[8,8,32] = \n" << (this->trace_format?std::hex:std::dec) << std::setw(2) << xt::view(this->x_buffer,xt::all(),xt::all(),xt::all()) << std::dec << "\n";
+    stringStream << "x_buffer[8,8,32] = \n" << (this->trace_format?std::hex:std::dec) << std::setw(2) << xt::view(this->x_buffer,xt::range(0,2),xt::range(0,2),xt::range(0,1)) << std::dec << "\n";
+    // stringStream << "x_buffer[8,8,32] = \n" << (this->trace_format?std::hex:std::dec) << std::setw(2) << xt::view(this->x_buffer,xt::all(),xt::all(),xt::all()) << std::dec << "\n";
     std::string copyOfStr = stringStream.str();
     this->trace.msg(vp::trace::LEVEL_DEBUG, copyOfStr.c_str());
 }
 
 void Neureka::debug_x_array() {
     std::ostringstream stringStream;
-    stringStream << "x_array[36,9,32] = \n" << xt::print_options::threshold(10000) << (this->trace_format?std::hex:std::dec) << std::setw(2) << xt::view(this->x_array,xt::all(),xt::all(),xt::all()) << std::dec << "\n";
+    stringStream << "x_array[36,9,32] = \n" << xt::print_options::threshold(10000) << (this->trace_format?std::hex:std::dec) << std::setw(2) << xt::view(this->x_array,xt::range(0,2),xt::all(),xt::range(0,1)) << std::dec << "\n";
     std::string copyOfStr = stringStream.str();
     this->trace.msg(vp::trace::LEVEL_DEBUG, copyOfStr.c_str());
+
+    stringStream << "x_array[36,9,32] = \n" << xt::print_options::threshold(10000) << (this->trace_format?std::hex:std::dec) << std::setw(2) << xt::view(this->x_array,xt::range(6,8),xt::all(),xt::range(0,1)) << std::dec << "\n";
+    std::string copyOfStr2 = stringStream.str();
+    this->trace.msg(vp::trace::LEVEL_DEBUG, copyOfStr2.c_str());
 }
 
 void Neureka::debug_accum(){
   std::ostringstream stringStream;
-  stringStream << "accum[32,36] = \n" << (this->trace_format?std::hex:std::dec) << std::setw(8) << xt::view(this->accum,xt::all(),xt::all()) << std::dec << "\n";
+  stringStream << "accum[32,36] = \n" << (this->trace_format?std::hex:std::dec) << std::setw(8) << xt::view(this->accum,xt::range(0,1),xt::range(0,2)) << std::dec << "\n";
   std::string copyOfStr = stringStream.str();
   this->trace.msg(vp::trace::LEVEL_DEBUG, copyOfStr.c_str());
+  stringStream << "accum[32,36] = \n" << (this->trace_format?std::hex:std::dec) << std::setw(8) << xt::view(this->accum,xt::range(0,1),xt::range(6,8)) << std::dec << "\n";
+  std::string copyOfStr2 = stringStream.str();
+  this->trace.msg(vp::trace::LEVEL_DEBUG, copyOfStr2.c_str());
 }
 
 void Neureka::debug_psum_block(){
