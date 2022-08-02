ec7cbfea3609c44a178420779f81320a05c08ba4
Arpan Prasad <prasadar@iis.ee.ethz.ch>
2022-08-02 15:11:30 +0200

    modified the application to make it compatible for both gvsoc and rtl. the padmuxing is only enabled for rtl model

--- tests/hello/test.c
+++ tests/hello/test.c
@@ -9,8 +9,9 @@
  */
 
 #include "pmsis.h"
-#include "siracusa_padctrl.h"
-
+#if defined(RTL_PLATFORM)
+    #include "siracusa_padctrl.h"
+#endif
 #if defined(CLUSTER)
 void pe_entry(void *arg)
 {
@@ -56,7 +57,9 @@ static void test_kickoff(void *arg)
 
 int main()
 {
-    padctrl_mode_set(PAD_GPIO42, PAD_MODE_UART0_TX);
-    padctrl_mode_set(PAD_GPIO41, PAD_MODE_UART0_RX);
+    #if defined(RTL_PLATFORM)
+        padctrl_mode_set(PAD_GPIO42, PAD_MODE_UART0_TX);
+        padctrl_mode_set(PAD_GPIO41, PAD_MODE_UART0_RX);
+    #endif
     return pmsis_kickoff((void *)test_kickoff);
 }
