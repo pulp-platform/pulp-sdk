7b13d26ab976158bf45288bda86a21b12332c76f
arpansur <minarpan@gmail.com>
2022-08-29 17:55:33 +0200

    ported pulp-runtime to pulp-sdk(tutorial developed by manuel)

--- tests/neureka_L1_patch/no_normquant_stride2_output_64x16x16_input_64x32x32/inc/neureka_scale_shift.h
+++
@@ -1,3 +0,0 @@
-#define STIM_NQS_SIZE 1
-// scale_shift [64]
-PI_L1 int8_t neureka_scale_shift_no_normquant[] = { 0 };
--- tests/neureka_L1_patch_debug/ki64_ko64_ho32_wo32_avgpool/inc/ne16_scale_bias.h
+++
@@ -1,3 +0,0 @@
-#define STIM_NQB_SIZE 4
-// scale_bias [64]
-PI_L1 int32_t ne16_scale_bias[] = { 0 };
