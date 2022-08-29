35f7be3c559b24b6bba31d1a01b34d5d272c2bbc
arpansur <minarpan@gmail.com>
2022-08-29 17:48:37 +0200

    moved neureka tests to a single folder to improve visibility of tests folder

---
+++ tests/neureka/1x1x32x1_st1_fs3/inc/neureka_scale.h
@@ -0,0 +1,6 @@
+#define STIM_NQ_SIZE 8
+// scale [1]
+int32_t __attribute__((section(".heapsram"))) neureka_scale[] = {
+  0x0001c484
+};
+
---
+++ tests/neureka/7x8x32x39_woffs1_fs3/inc/neureka_scale_shift.h
@@ -0,0 +1,3 @@
+#define STIM_NQS_SIZE 1
+// scale_shift [39]
+int8_t __attribute__((section(".heapsram"))) neureka_scale_shift[] = { 0 };
