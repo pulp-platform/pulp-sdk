ca12357dce777a115b5f067dbc2f937c7b081b97
Germain Haugou <germain.haugou@greenwaves-technologies.com>
2022-11-28 20:54:51 +0100

    [GVSOC] Updated to latest version

--- configs/common.sh
+++ configs/common.sh
@@ -40,10 +40,9 @@ export GVSOC_INC_PATHS="$PULP_SDK_HOME/rtos/pulpos/gap_archi/include/archi/chips
 export GVSOC_ISS_PATH=$PULP_SDK_HOME/tools/gvsoc/common/models/cpu/iss
 export GVSOC_SRC_PATH=$PULP_SDK_HOME/tools/gvsoc/common
 export GVSOC_PULP_SRC_PATH=$PULP_SDK_HOME/tools/gvsoc/pulp
+export PYTHONPATH=$PULP_SDK_HOME/tools/gvsoc/common/models:$PYTHONPATH
 export PYTHONPATH=$PULP_SDK_HOME/tools/gvsoc/pulp/models:$PYTHONPATH
-export PYTHONPATH=$PULP_SDK_HOME/tools/gvsoc/pulp/generators:$PYTHONPATH
-export PYTHONPATH=$PULP_SDK_HOME/tools/gvsoc/common/generators:$PYTHONPATH
-export PYTHONPATH=$PULP_SDK_HOME/tools/gvsoc/common/engine/python:$PYTHONPATH
+export PYTHONPATH=$PULP_SDK_HOME/tools/gvsoc/common/engine:$PYTHONPATH
 
 # FPGA
 #export PULP_
