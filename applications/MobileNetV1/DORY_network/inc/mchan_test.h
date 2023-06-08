101fc0281a39805ceac8b0c9d34080497981d681
Luca <luca.valente@unibo.it>
2023-06-08 14:36:49 +0200

    Fix dma address for pulp

--- applications/MobileNetV1/DORY_network/inc/mchan_test.h
+++ applications/MobileNetV1/DORY_network/inc/mchan_test.h
@@ -185,8 +185,8 @@ static  __attribute__ ((noinline))  void pe_mchan_free(int id) {
 
 #define PLATFORM 'FPGA'
 
-#define MCHAN_COMMAND_QUEUE    0x10201800
-#define MCHAN_STATUS_REGISTER  0x10201804
+#define MCHAN_COMMAND_QUEUE    0x10204400
+#define MCHAN_STATUS_REGISTER  0x10204404
 
 // TEMPORARY FIX DAVIDE
 #define PLP_DMA_2D_TCDM_BIT 22
